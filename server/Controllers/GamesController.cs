d159124ef3ee12930b55ee551cad223ea11bb925
aldillek <aldillek@gmail.com>
2022-08-26 20:29:48 +0200

    Added params to get games.

--- server/Controllers/GamesController.cs
+++ server/Controllers/GamesController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -12,6 +14,7 @@ namespace server.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class GamesController : ControllerBase
     {
         private readonly DatabaseContext _context;
@@ -23,13 +26,26 @@ namespace server.Controllers
 
         // GET: api/Games
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Game>>> GetGames()
+        public async Task<ActionResult<IEnumerable<Game>>> GetGames([FromQuery]string? skip)
         {
+            
+            var alreadyLoaded = 0;
+
+            try
+            {
+                int.TryParse(skip, out alreadyLoaded);
+            }
+            catch
+            {
+                alreadyLoaded = 0;
+            }
+            
+
           if (_context.Games == null)
           {
               return NotFound();
           }
-            return await _context.Games.ToListAsync();
+            return await _context.Games.Skip(alreadyLoaded).Take(30).ToListAsync();
         }
 
         // GET: api/Games/5
@@ -84,24 +100,20 @@ namespace server.Controllers
         // POST: api/Games
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Game>> PostGame(List<Game> games)
+        public async Task<ActionResult<Game>> PostGame(Game game)
         {
 
-
-            
           if (_context.Games == null)
           {
               return Problem("Entity set 'DatabaseContext.Games'  is null.");
           }
-            foreach (Game game in games)
-            {
-                
-                await _context.Games.AddAsync(game);
-            }
+            
+            var addedGame =  await _context.Games.AddAsync(game);
+            
 
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetGame", new { id = games.FirstOrDefault().Id }, games.FirstOrDefault());
+            return CreatedAtAction("GetGame", new { id = addedGame.Entity.Id }, addedGame.Entity);
         }
 
         // DELETE: api/Games/5
